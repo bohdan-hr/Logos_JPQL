2426bef41f26fe7fc3b5fd79d2b10512e06743e7
bohdan-hr <bohdan.h@ex.ua>
2018-03-27 18:56:37 +0300

    add createQuery

--- src/main/java/Less7/App.java
+++ src/main/java/Less7/App.java
@@ -1,6 +1,7 @@
 package Less7;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -25,10 +26,50 @@ public class App
     	//List<Comment> comments = em.createQuery("SELECT c FROM Comment c", Comment.class).getResultList();
     	//comments.forEach(c -> System.out.println(c));
     	
-    	Comment commentById = em.createQuery("SELECT c FROM Comment c WHERE c.id = :comm_id",  Comment.class)
-    			.setParameter("comm_id", 40).getSingleResult();
     	
-    	System.out.println(commentById);
+    	//Comment commentById = em.createQuery("SELECT c FROM Comment c WHERE c.id = :comm_id",  Comment.class)
+    	//		.setParameter("comm_id", 40).getSingleResult();
+    	//System.out.println(commentById);
+    	
+    	
+    	//List<Post> posts = em.createQuery("SELECT p FROM Post p WHERE p.id > :post_id",  Post.class)
+    	//		.setParameter("post_id", 50).getResultList();
+    	//posts.forEach(p -> System.out.println(p));
+    	
+    	
+    	//List<Post> posts = em.createQuery("SELECT p FROM Post p WHERE p.id IN (:post_ids)", Post.class)
+    	//		.setParameter("post_ids", Arrays.asList(2,56,62,3,88,95)).getResultList();
+    	//posts.forEach(p -> System.out.println(p));
+    	
+    	
+    	//List<Post> posts = em.createQuery("SELECT p FROM Post p WHERE p.title LIKE :post_title", Post.class)
+    	//		.setParameter("post_title", "%8").getResultList(); 
+    	//posts.forEach(p -> System.out.println(p));
+    	
+    	
+    	//List<Post> posts2 = em.createQuery("SELECT p FROM Post p WHERE p.title LIKE :post_title", Post.class)
+    	//		.setParameter("post_title", "%8_").getResultList(); 
+    	//posts2.forEach(p -> System.out.println(p));
+    	
+    	
+    	//List<Post> posts2 = em.createQuery("SELECT p FROM Post p WHERE p.id BETWEEN :first AND :last", Post.class)
+    	//		.setParameter("first", 76).setParameter("last", 82).getResultList(); 
+    	//posts2.forEach(p -> System.out.println(p));
+    	
+    	//Agreg function
+    	Long count = em.createQuery("SELECT count(c.id) FROM Comment c", Long.class).getSingleResult();
+    	System.out.println("Count " + count);
+    	
+    	Long sum = em.createQuery("SELECT sum(c.id) FROM Comment c", Long.class).getSingleResult();
+    	System.out.println("Sum " + sum);
+    	
+    	Double avg = em.createQuery("SELECT avg(c.id) FROM Comment c", Double.class).getSingleResult();
+    	System.out.println("AVG " + avg);
+    	
+    	Integer max = em.createQuery("SELECT max(c.id) FROM Comment c", Integer.class).getSingleResult();
+    	System.out.println("Max " + max);
+    	Integer min = em.createQuery("SELECT min(c.id) FROM Comment c", Integer.class).getSingleResult();
+    	System.out.println("Min " + min);
     	
     	//addTags(em);
     	//addPost(em);
